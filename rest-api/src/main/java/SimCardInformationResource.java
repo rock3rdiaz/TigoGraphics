9c149e1538aaf447f4646cd2452c62d39e7ba9e7
Alexander Galvis <galcorp@gmail.com>
2018-04-27 11:33:44 -0600

    Configuracion gradle

---
+++ rest-api/src/main/java/SimCardInformationResource.java
@@ -0,0 +1,12 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/")
+public class SimCardInformationResource {
+
+	@GET
+	@Path("/get-information")
+	public void getSimCardInformation() {
+		
+	}
+}
